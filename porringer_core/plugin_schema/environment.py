a0235e2f14716bff4e71204597ca0194e96b82f5
Asher Norland <asher.norland@gmail.com>
2023-08-26 05:24:59 -0400

    Action Parameters

--- porringer_core/plugin_schema/environment.py
+++ porringer_core/plugin_schema/environment.py
@@ -3,7 +3,38 @@
 from abc import abstractmethod
 from typing import Protocol
 
+from pydantic import BaseModel, Field
+
 from porringer_core.schema import Package, PackageName
+
+
+class InstallParameters(BaseModel):
+    """The install parameters for an environment plugin"""
+
+    name: PackageName = Field(description="The package to install")
+    dry: bool = Field(
+        default=False, description="If True, rehearses an installation without modifying what is actually installed"
+    )
+
+
+class UninstallParameters(BaseModel):
+    """The uninstall parameters for an environment plugin"""
+
+    names: list[PackageName] = Field(
+        description="The list of packages to uninstall. If empty, all packages are uninstalled"
+    )
+    dry: bool = Field(
+        default=False, description="If True, rehearses an uninstall without modifying what is actually installed"
+    )
+
+
+class UpgradeParameters(BaseModel):
+    """The upgrade parameters for an environment plugin"""
+
+    names: list[PackageName] = Field(description="The list of packages to upgrade. If empty, all packages are upgraded")
+    dry: bool = Field(
+        default=False, description="If True, rehearses an upgrade without modifying what is actually installed"
+    )
 
 
 class EnvironmentPlugin(Protocol):
@@ -31,11 +62,11 @@ class EnvironmentPlugin(Protocol):
         raise NotImplementedError
 
     @abstractmethod
-    def install(self, name: PackageName) -> Package | None:
+    def install(self, params: InstallParameters) -> Package | None:
         """Installs the given package identified by its name
 
         Args:
-            name: The package name to install
+            params: The installation parameters
 
         Returns:
             The package, or None if it doesn't exist
@@ -43,11 +74,11 @@ class EnvironmentPlugin(Protocol):
         raise NotImplementedError
 
     @abstractmethod
-    def uninstall(self, names: list[PackageName]) -> list[Package | None]:
+    def uninstall(self, params: UninstallParameters) -> list[Package | None]:
         """Uninstalls the given list of packages
 
         Args:
-            names: The packages to uninstall
+            params: The uninstall parameters
 
         Returns:
             A list of packages that were uninstalled. Each item could be None if there was a failure
@@ -55,11 +86,11 @@ class EnvironmentPlugin(Protocol):
         raise NotImplementedError
 
     @abstractmethod
-    def upgrade(self, names: list[PackageName]) -> list[Package | None]:
+    def upgrade(self, params: UpgradeParameters) -> list[Package | None]:
         """Upgrades the given list of packages
 
         Args:
-            names: The packages to upgrade
+            params: The upgrade parameters
 
         Returns:
             A list of packages that were upgraded. Each item could be None if there was a failure
